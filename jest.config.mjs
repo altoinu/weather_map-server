5c00bcb7d0ec860850e75bf9958729001af17201
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2025-02-06 23:39:14 -0500

    notes, clean up for jest set up

--- jest.config.mjs
+++ jest.config.mjs
@@ -137,7 +137,7 @@ const config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.mjs"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -203,4 +203,4 @@ const config = {
   // watchman: true,
 };
 
-module.exports = config;
+export default config;
